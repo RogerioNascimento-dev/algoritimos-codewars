7457131bf45623f11c9c6a03100c9e24862eaa74
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-27 14:33:47 -0300

    return string in default camelCase

--- ConvertStringToCamelCase/Program.cs
+++ ConvertStringToCamelCase/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+Console.WriteLine(ToCamelCase("the-stealth-warrior"));
+Console.WriteLine(ToCamelCaseCodeWars("the-stealth-warrior"));
+string ToCamelCase(string str)
+{
+
+    
+    char[] ArrayStr = str.ToCharArray();
+
+    foreach(var New in str.Select((value, index) => new { value, index }))
+    {
+        if (New.value == '-' || New.value == '_')        
+            ArrayStr[New.index +1] = char.ToUpper(ArrayStr[New.index + 1]);       
+            
+    }
+    return new String(ArrayStr).Replace("-","").Replace("_","");        
+    
+}
+
+//Solução da plataforma
+string ToCamelCaseCodeWars(string str)
+{
+    return Regex.Replace(str, @"[_-](\w)", m => m.Groups[1].Value.ToUpper());
+}
