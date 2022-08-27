f173d4463c9e93e31c6e8e92e21739065bf986dc
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-27 12:29:50 -0300

    BinaryInterval problem

---
+++ BinaryInterval/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+
+string interval = "";
+string controlInterval = "C";
+List<string> intervals = new List<string>();
+
+
+Console.WriteLine(BinaryInterval(1041));
+
+int BinaryInterval(int N)
+{
+    string binary = Convert.ToString(N, 2);
+
+    foreach (var New in binary.Select((value, index) => new { value, index }))
+    {
+        if (New.value == '1' && controlInterval == "C")
+        {
+            OpenInterval();
+            continue;
+        }
+        if (New.value == '1' && controlInterval == "O")
+        {
+            CloseInterval();
+            continue;
+        }
+        if (New.value == '0')
+        {
+            interval += New.value;
+            continue;
+        }
+    }
+
+    var final = intervals.Count() > 0 ? intervals.Max().Length : 0;
+    return final;
+}
+
+void OpenInterval()
+{
+    if (interval != "")
+        intervals.Add(interval);
+
+    interval = "";
+    controlInterval = "O";
+}
+
+void CloseInterval()
+{
+    if (interval != "")
+        intervals.Add(interval);
+    controlInterval = "C";
+    interval = "";
+}
