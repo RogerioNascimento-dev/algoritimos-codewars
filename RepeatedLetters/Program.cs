dc233d97a0a6e40978d1f3dc352a79846c37be2c
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-27 11:57:48 -0300

    First Commit With code problem Repeat Letters

---
+++ RepeatedLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿string RepeatedLetters(string A)
+{
+    string found = null;
+    foreach (var New in A.Select((value, index) => new { value, index }))
+    {
+
+        if (found == null && A.Any(ale => ale == New.value) && isDifferentIndex(A, New.value, New.index))
+        {
+            found = New.value.ToString();
+        }
+    }
+    found = found == null ? A[0].ToString() : found;
+    return found;
+}
+
+bool isDifferentIndex(string A, char value, int currentindex)
+{
+    int foundIndex = Array.IndexOf(A.ToCharArray(), value);
+    return foundIndex != currentindex;
+}
+
+Console.WriteLine(RepeatedLetters("hello"));
