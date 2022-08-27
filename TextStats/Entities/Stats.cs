37407e249fb959664b42b24985881394be5d0770
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-27 12:24:40 -0300

    Text Stats problem

---
+++ TextStats/Entities/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStats.Entities
+{
+    public class Stats
+    {
+        // Total number of all words in the document
+        public int NumberOfAllWords { get; set; }
+
+        // Returns number of words that consist only from digits e.g. 13455, 98374
+        public int NumberOfWordsThatContainOnlyDigits { get; set; }
+
+        // Returns number of words that start with a lower letter e.g. a, d, z
+        public int NumberOfWordsStartingWithSmallLetter { get; set; }
+
+        // Returns number of words that start with a capital letter e.g. A, D, Z
+        public int NumberOfWordsStartingWithCapitalLetter { get; set; }
+
+        // Returns the first longest word in the document
+        public string TheLongestWord { get; set; }
+
+        // Returns the first shortest word in the document
+        public string TheShortestWord { get; set; }
+    }
+}
---
+++ TextStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using TextStats.Entities;
+using System.Text.RegularExpressions;
+using System.Text.Json;
+
+
+Stats TextStats(string document)
+{
+    if (document == null)
+        throw new ArgumentNullException();
+
+    Stats stats = new Stats();
+
+    stats.NumberOfAllWords = document.Split(' ').Where(s => s != "").Count();
+    stats.NumberOfWordsThatContainOnlyDigits = document.Split(' ').Where(s => s != "").Where(s => Regex.IsMatch(s, @"^[0-9]+$")).Count();
+    stats.NumberOfWordsStartingWithSmallLetter = document.Split(' ').Where(s => s != "").Where(s => char.IsLower(s[0])).Count();
+    stats.NumberOfWordsStartingWithCapitalLetter = document.Split(' ').Where(s => s != "").Where(s => char.IsUpper(s[0])).Count();
+    stats.TheLongestWord = document.Split(' ').Where(s => s != "").GroupBy(s => s.Length).OrderByDescending(s => s.Key).Select(s => s.First()).FirstOrDefault();
+    stats.TheShortestWord = document.Split(' ').Where(s => s != "").GroupBy(s => s.Length).OrderBy(s => s.Key).Select(s => s.First()).FirstOrDefault();
+    
+    return stats;
+}
+
+Console.WriteLine(JsonSerializer.Serialize(TextStats("testando PEIXE BOLA E GATO")));
