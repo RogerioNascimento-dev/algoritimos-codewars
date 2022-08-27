8e46dff3c075935081373ee99c63e7375df161fa
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-27 15:36:13 -0300

    understanding even more regex

--- TrainingOfRegex/Program.cs
+++ TrainingOfRegex/Program.cs
@@ -2,9 +2,19 @@
 
 void Training(string data)
 {
-    var rgx = new Regex(@"Dia (\d{2})\/(\d{2})\/(\d{4})");    
+    var rgx = new Regex(@"Dia (\d{2})\/(\d{2})\/(\d+)");    
     var primeiro = rgx.Match(data);
     var Encontrados = rgx.Matches(data);
+
+
+    string Url = "https://www.rogerionascimento.dev.br/skills/88";
+    var rgxRota = new Regex(@"https://.*\.\w{2}\/(\w+)\/(.*)");
+    var rgxRotaMatch = rgxRota.Match(Url);
+    var rota = rgxRota.Replace(Url, e => e.Groups[1].Value);
+
+
+
+    var a = "";
 }
 
 
