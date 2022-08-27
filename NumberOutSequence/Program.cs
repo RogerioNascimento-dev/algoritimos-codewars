b8cdb815c021b126aab7dfd23fc6be38c23d7de3
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-27 12:45:45 -0300

    return the next number missing sequence bigger then zero or next number of sequence

---
+++ NumberOutSequence/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+Console.WriteLine(NumberOutSequence(new int[] { 1, 3, 6, 5, 4, 1, 2 }));
+
+/*
+ * Menor numero restante na sequencia maior q 0 ou próximo número
+ */
+int NumberOutSequence(int[] A)
+{
+    Array.Sort(A);
+
+
+    int ret = 1;
+    bool found = false;
+    if (A[A.Length - 1] < 0)
+    {
+        ret = 1;
+    }
+    else
+    {
+        int cont = 0;
+        while (!found)
+        {
+            if (!A.Any(ale => ale == A[cont] + 1))
+            {
+                ret = A[cont] + 1;
+                found = true;
+            }
+            cont++;
+        };
+    }
+
+    return ret;
+}
