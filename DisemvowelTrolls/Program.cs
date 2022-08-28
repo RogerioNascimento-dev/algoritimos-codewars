a02886ec4c1badedf93298a84ea87c6a5144e0aa
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-28 14:34:48 -0300

    implement problem Disemvowel, return words without vowel

---
+++ DisemvowelTrolls/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine(Disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
+
+string Disemvowel(string str)
+{    
+    return Regex.Replace(str,@"([aeiouAEIOU]+)",r => "");
+}
