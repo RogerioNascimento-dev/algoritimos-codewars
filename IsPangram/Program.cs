1e796be9f90e929f502059e2749c965d3a2b852d
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-28 17:00:24 -0300

    problem resolved, return true if string input have all the letters of the alphabet

---
+++ IsPangram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+Console.WriteLine(IsPangram("The quick brown fox jumps over the lazy dog"));
+
+bool IsPangram(string str)
+{
+    /* Retorno decomposto;    
+    var matches = Regex.Matches(str, @"[a-zA-Z]+");
+    var resumido = String.Join("", matches);
+    var minusculo = resumido.ToLower();
+    var distincted = minusculo.OrderBy(e =>e).Distinct();    
+    var TotalDistincted = distincted.Count();
+    return true;
+    */
+    return String.Join("", Regex.Matches(str, @"[a-zA-Z]+")).ToLower().Distinct().Count() == 26;
+}
