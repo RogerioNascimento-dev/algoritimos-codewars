b4c9e66cf29a73b06d2ffbfce0261a8df3ee66ea
Rogéario Nascimento <rogerio.nascimento@soterotech.com.br>
2022-08-28 17:18:37 -0300

    implement problem CategorizeNewMember, return Open or Senior list

---
+++ CategorizeNewMember/Program.cs
@@ -0,0 +1,14 @@
+﻿
+OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } });
+IEnumerable<string> OpenOrSenior(int[][] data)
+{
+    List<string> response = new List<string>();
+    foreach (var i in data)
+    {
+        if (i[0] >= 55 && i[1] > 7)
+            response.Add("Senior");
+        else
+            response.Add("Open");
+    }
+    return response;
+}
